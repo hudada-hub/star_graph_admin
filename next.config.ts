e07013c3e7992f16dc541ee2ae00d4fb5c889e07
HUHU <wzqwzq10122216@qq.com>
2025-08-16 00:03:40 +0800

    恢复数据

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    // 设置端口号
+    env: {
+        PORT: '3001'
+    },
+    // 或者使用这种方式设置端口
+    experimental: {
+        serverComponentsExternalPackages: []
+    },
     images: {
         remotePatterns: [
           {
